5faaf99a6d5ed30b00b7e0325a0b1231faafe0d9
DTH235768-NguyenChiThanh <thanh_dth235768@student.agu.edu.vn>
2026-04-09 13:52:21 +0700

    Finíhed

--- modules/firstimage.js
+++ modules/firstimage.js
@@ -1,7 +1,11 @@
 function firstImage(noiDung) {
 	var regExp = /<img[^>]+src="?([^"\s]+)"?[^>]*\/>/g;
 	var results = regExp.exec(noiDung);
+<<<<<<< HEAD
 	var image = 'http://127.0.0.1:3000/images/noimage.png';
+=======
+	var image = 'https://trangtin-eypw.onrender.com/images/noimage.png';
+>>>>>>> e8320e8 (add)
 	if(results) image = results[1];
 	return image;
 }
